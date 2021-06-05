064a3757e3c7e9bf43e741df8d2e64747697179b
vallabhppatil <37295340+vallabhppatil@users.noreply.github.com>
2021-06-05 11:14:08 +0530

    Add files via upload

---
+++ FET/Js Assign/Hello.js
@@ -0,0 +1,4 @@
+function sayHello()
+{
+return string “Hello from JavaScript“
+}
---
+++ FET/Js Assign/assign4.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script>
+function hello(){
+for (var input = 1; input <= 10; input++) {
+ console.log(input);
+}
+}
+</script>
+<body>
+<button type="submit" onclick="hello()">
+</body>
+</html>
